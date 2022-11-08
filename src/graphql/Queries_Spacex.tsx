c43381ee239a0a1ce35ef2df9de7e55d8617f0ff
rgzahir <zahir.ayob@regovtech.com>
2022-11-08 17:21:59 +0800

    update queries and functions

--- src/graphql/Queries_Spacex.tsx
+++ src/graphql/Queries_Spacex.tsx
@@ -2,25 +2,9 @@ import { gql } from "@apollo/client";
 
 export const GET_SPACEX_QUERY = gql`
   query getDataByAmount {
-    launchesPast(limit: 20) {
+    launchesPast(limit: 100) {
       mission_name
       launch_date_local
     }
   }
 `;
-
-// launch_site {
-//     site_name_long
-//   }
-//   links {
-//     article_link
-//     video_link
-//   }
-//   rocket {
-//     rocket_name
-//   }
-//   ships {
-//     name
-//     home_port
-//     image
-//   }
