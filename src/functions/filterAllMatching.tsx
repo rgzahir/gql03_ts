6d449758d73402a3f326e4d2b5b7aeb0c263d1ea
rgzahir <zahir.ayob@regovtech.com>
2022-11-08 16:50:40 +0800

    filter all matching feature

--- src/functions/filterAllMatching.tsx
+++ src/functions/filterAllMatching.tsx
@@ -1,4 +1,4 @@
-export const FilterAllMatching = (data: any | undefined, finder: string) => {
+export const FilterAllMatching = (data: any, finder: string) => {
   if (data) {
     const { launchesPast } = data;
 
@@ -9,11 +9,11 @@ export const FilterAllMatching = (data: any | undefined, finder: string) => {
     );
 
     // run console log
-    console.log(`typeof launchesPast:  ${typeof launchesPast}`);
+    //   console.log(`typeof launchesPast:  ${typeof launchesPast}`);
     // console.log(Object.keys(found).length);
 
     //result
-    if (res === undefined || res.length == 0) {
+    if (res === undefined || res.length === 0) {
       //   console.log(res.mission_name); //show undefined
 
       console.log(`RES IS UNDEFINED OR EMPTY`);
@@ -23,12 +23,28 @@ export const FilterAllMatching = (data: any | undefined, finder: string) => {
       //   console.log(res);
       //   console.log(Object.keys(res).length);
 
-      var resArray = Object.keys(res).map((key) => [Number(key), res[key]]);
-      console.log(`typeof resArray:  ${typeof resArray}`);
+      // var resArray = Object.keys(res).map((key) => [Number(key), res[key]]);
+      // console.log(`typeof resArray:  ${typeof resArray}`);
 
       return res;
     }
   }
+
+  // if (data && finder !== "") {
+  //   data.map((item, key) => {
+  //     return (
+  //       <View>
+  //         <TouchableOpacity key={key} style={{ paddingLeft: 16 }}>
+  //           <Text>{item.mission_name}</Text>
+  //         </TouchableOpacity>
+
+  //         {/* <LocalModal item={item} /> */}
+  //       </View>
+  //     );
+  //   });
+  // } else {
+  //   <View />;
+  // }
 };
 
 export default FilterAllMatching;
